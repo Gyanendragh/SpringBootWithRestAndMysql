5b3430b4b18413c8a4c2a3c3b03360679dedd678
Gyanendragh <gyanendraghimire4@gmail.com>
2017-02-26 15:00:09 -0600

    first commit

--- src/main/java/com/example/repository/PersonRepository.java
+++ src/main/java/com/example/repository/PersonRepository.java
@@ -3,6 +3,6 @@ package com.example.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.domain.Person;
 
-public interface PersonRepository extends JpaRepository<Person, Long> { /*as long is primitive so we use Long*/
+public interface PersonRepository extends JpaRepository<Person, Long> { 
 
 }
